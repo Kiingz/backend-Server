139c6d733273808cfe7cb9e14258aad8817cd9e5
Alejandro Reyes <reiies094@gmail.com>
2019-03-14 17:52:19 -0300

    Upload Imagenes al servidor

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var medicoRoutes = require('./routes/medico');
 var hospitalRoutes = require('./routes/hospital');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var uploadRoutes = require('./routes/upload');
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -29,6 +30,8 @@ app.use('/usuario', usuarioRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/hospital', hospitalRoutes);
 app.use('/login', loginRoutes);
+app.use('/upload', uploadRoutes);
+
 app.use('/', appRoutes);
 
 // Escuchar peticiones
