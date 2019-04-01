086ae47dc5f69cf2455ab77166369bb5eec4be81
Alejandro Reyes <reiies094@gmail.com>
2019-04-01 18:26:37 -0300

    Cambios finales - Rev busqueda general

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -6,23 +6,45 @@ var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
 var Usuario = require('../models/usuario');
 
+// Busqueda por coleccion
+app.get('/coleccion/:tabla/:busqueda', (req, res) => {
+    var tabla = req.params.tabla;
+    var busqueda = req.params.busqueda;
+    // Expresion regular, insensible a mayusculas y minusculas
+    var regex = new RegExp(busqueda, 'i');
+    var promesa;
+
+    switch (tabla) {
+        case 'usuarios':
+            promesa = buscarUsuario(busqueda, regex);
+            break;
+        case 'medicos':
+            promesa = buscarMedicos(busqueda, regex);
+            break;
+        case 'hospitales':
+            promesa = buscarHospitales(busqueda, regex);
+            break;
+
+        default:
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Los tipos de busqueda solo son: usuario, medicos y hospitales',
+                error: { message: 'Tipo de tabla/coleccion no valido' }
+            });
+    }
+    promesa.then(data => {
+        res.status(200).json({
+            ok: true,
+            [tabla]: data
+        });
+    });
+});
+
+// Busqueda General
 app.get('/todo/:busqueda', (req, res, next) => {
     var busqueda = req.params.busqueda;
     // Expresion regular, insensible a mayusculas y minusculas
     var regex = new RegExp(busqueda, 'i');
-
-    Promise.all([
-        buscarHospitales(busqueda, regex),
-        buscarMedicos(busqueda, regex),
-        buscarUsuario(busqueda, regex)
-    ]).then((respuestas) => {
-        res.status(200).json({
-            ok: true,
-            hospitales: respuestas[0],
-            medicos: respuestas[1],
-            usuarios: respuestas[2]
-        });
-    });
 });
 
 function buscarHospitales(busqueda, regex) {
